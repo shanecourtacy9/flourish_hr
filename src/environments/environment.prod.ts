f2de0cd9c2ef32e9b4b0538509886392fc6d1a96
shanemkhize <shane.wmkhize@cloudseed.io>
2025-10-28 10:07:48 +0200

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // url: "https://production-dot-uphealthgcp.et.r.appspot.com",
-  url: "https://staging-dot-uphealthgcp.et.r.appspot.com",
+  url: "https://production-dot-uphealthgcp.et.r.appspot.com",
+  // url: "https://staging-dot-uphealthgcp.et.r.appspot.com",
 };
